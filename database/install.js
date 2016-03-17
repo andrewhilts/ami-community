6b501748c36c5f0f7665aef55f634bf3484f9072
Andrew Hilts <andrewchilts@gmail.com>
2016-03-17 16:08:55 -0400

    close db connecti

--- database/install.js
+++ database/install.js
@@ -8,12 +8,15 @@ var Event = require('../models/event').EventController(bookshelf);
 fs.readFile(installQueryFile, "utf-8", function(err, installQuery){
 	if(!err){
 		bookshelf.knex.raw(installQuery).then(function(resp){
+			 bookshelf.knex.destroy();
 			console.log(null, resp);
 		}).catch(function(error){
+			bookshelf.knex.destroy();
 			console.log(error);
 		});
 	}
 	else{
+		bookshelf.knex.destroy();
 		console.log(err);
 	}
 });
