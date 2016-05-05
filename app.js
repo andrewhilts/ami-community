fded3d3e950a0ab188bad843ffe1dbf688acd306
Andrew Hilts <andrewchilts@gmail.com>
2016-05-05 14:27:15 +0800

    middleware testing

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var Email = require('./models/email.js').EmailModel;
 var Feedback = require('./models/feedback.js').FeedbackModel(bookshelf);
 var policy = require('./conf/policy.conf').policy;
 var uuid = require('node-uuid');
+
 var app = express();
+var router = express.Router();
 
 var limiter = rateLimit({
 	windowMS: 60000,
@@ -31,29 +33,34 @@ var feedbackController = require('./controllers/feedbackController/index.js').fe
 var unsubscribeController = require('./controllers/unsubscribeController/index.js').unsubscribeController(Subscription);
 // var parseForm = bodyParser.urlencoded({ extended: false })
 
+
 app.set('port', process.env.PORT || 3000);
 
-app.post('/enroll', function(){
-	return bodyParser.urlencoded({ extended: true })
-}, bodyParser.json, helmet, limiter, enrollmentController.submit);
-app.get('/verify', function(){
-	return bodyParser.urlencoded({ extended: true })
-}, bodyParser.json, helmet, limiter, enrollmentController.verifyAndEnroll);
-// app.get('/feedback', feedbackController.getForm);
-app.post('/feedback', function(){
-	return bodyParser.urlencoded({ extended: true })
-}, bodyParser.json, helmet, limiter, feedbackController.submit);
-app.post('/unsubscribe', function(){
-	return bodyParser.urlencoded({ extended: true })
-}, bodyParser.json, helmet, limiter, unsubscribeController.unsubHandler);
 
-var myLogger = function (err, req, res, next) {
+app.use(helmet());
+// app.use(cors({
+// 	origin: policy.AMIFrontEnd.baseURL
+// }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(limiter);
+
+router.post('/enroll', enrollmentController.submit);
+router.get('/verify', enrollmentController.verifyAndEnroll);
+// app.get('/feedback', feedbackController.getForm);
+router.post('/feedback', feedbackController.submit);
+router.post('/unsubscribe', unsubscribeController.unsubHandler);
+// app.use(cookieParser({''}));
+// app.use(csrf());
+
+var myErrorLogger = function (err, req, res, next) {
   console.log('error on request %s %s: %s', req.method, req.url, err);
   res.status(500).send("Something bad happened. :(");
+  res.end();
   process.exit(1);
 };
 
-app.use(myLogger);
+app.use(myErrorLogger);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
