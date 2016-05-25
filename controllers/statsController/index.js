3aafc8dc55de2a0754c7030273cd5ee639cccfbb
Andrew Hilts <andrewchilts@gmail.com>
2016-05-25 12:11:27 -0400

    debug

--- controllers/statsController/index.js
+++ controllers/statsController/index.js
@@ -89,7 +89,7 @@ var statsController = function(Request){
 	self.formatDates = function(dateRange){
 		var dates = Object.keys(dateRange);
 		for(var i=0; i < dates.length; i++){
-			console.log(i);
+			console.log(dateRange[dates[i]][0]);
 			// dateRange[i][0] = moment.utc(dateRange[i][0]).format('YYYY-MM-DD')
 		}
 		return dateRange;
