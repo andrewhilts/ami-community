7dc0e4a18ee909547e984e490bd5d814a8d8382b
Andrew Hilts <andrewchilts@gmail.com>
2016-05-25 11:44:47 -0400

    debug

--- controllers/statsController/index.js
+++ controllers/statsController/index.js
@@ -72,7 +72,7 @@ var statsController = function(Request){
 		var dates = new Request.RequestCollection()
 		.query(function(qb){
 			qb.where('operator_jurisdiction_id', jurisdiction_id);
-			qb.groupBy('operator_id');
+			qb.groupBy('request_date');
 			// qb.orderBy('request_date');
 		})
 		.fetch();
