4b016f0244869208265cbe8bd53599cb62386773
Andrew Hilts <andrewchilts@gmail.com>
2016-05-25 11:53:43 -0400

    debug

--- models/request.js
+++ models/request.js
@@ -7,7 +7,7 @@ var Request = function(bookshelf){
 		'tableName': 'requests',
 		'idAttribute': 'request_id',
 		parse: function(attrs){
-			attrs.request_date = attrs.request_date.format('YYYY-MM-DD')
+			attrs.request_date = moment(attrs.request_date).format('YYYY-MM-DD')
 		}
 	});
 	var RequestCollection = bookshelf.Collection.extend({
