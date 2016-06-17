11e05caa480c9a82ce8e6b565feed898c58ac4f7
Andrew Hilts <andrewchilts@gmail.com>
2016-06-17 12:32:18 -0400

    debug service

--- database/db.js
+++ database/db.js
@@ -1,6 +1,8 @@
 var settings = require('../conf/db.conf.js');
 console.log(settings.db_conn)
-console.log(process.cwd())
+console.log(process.version)
+console.log(process.env.USER)
+console.log(process.env.SHELL)
 var knex = require('knex')({
   client: 'pg',
   connection: settings.db_conn
