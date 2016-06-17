61f761a9242167319de9546f6e7783943dce9355
Andrew Hilts <andrewchilts@gmail.com>
2016-06-17 12:21:58 -0400

    debugging unsubscribe

--- database/db.js
+++ database/db.js
@@ -1,5 +1,6 @@
 var settings = require('../conf/db.conf.js');
 console.log(settings.db_conn)
+console.log(process.cwd())
 var knex = require('knex')({
   client: 'pg',
   connection: settings.db_conn
